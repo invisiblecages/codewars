2fbf13b574e62ea253bf9f75ae5c75572cf08f10
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2022-11-09 18:54:45 +0100

    added more katas

---
+++ 6kyu/Split Strings/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Complete the solution so that it splits the string into pairs of two characters. 
+If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+*/
+using System.Collections.Generic;
+
+public class SplitString
+{
+  public static string[] Solution(string str)
+  {
+    List<string> arr = new List<string>();
+    string res = "";
+    
+    for (int i = 0; i < str.Length; i++)
+    {      
+      res += str[i];
+      
+      if (res.Length == 2)
+      {
+        arr.Add(res);
+        res = "";
+      }
+      
+      if (str.Length % 2 != 0 && i == str.Length - 1)
+      { 
+        res += "_";
+        arr.Add(res);  
+      }
+    }
+    
+    return arr.ToArray();
+  }
+}
---
+++ 8kyu/Sum The Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+
+Notes:
+
+    If either input is an empty string, consider it as zero.
+
+    Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+*/
+using System;
+namespace Solution
+{
+  public static class Program
+  {
+    public static string StringsSum(string s1, string s2)
+    {
+      if (s1 == "")
+        s1 = "0";
+      if (s2 == "")
+        s2 = "0";
+          
+      return (int.Parse(s1) + int.Parse(s2)).ToString();
+    }
+  }
+}
