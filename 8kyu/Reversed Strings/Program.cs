4e1402cf4b81e5a6261868a19ff3f4c38e677525
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2022-11-17 22:57:07 +0100

    added some katas

---
+++ 8kyu/Reversed Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+public static class Kata
+{
+  public static string Solution(string str) 
+  {
+    string res = "";
+    for (int i = str.Length - 1; i >= 0; i--)
+      res += str[i];
+    
+    return res;
+  }
+}
