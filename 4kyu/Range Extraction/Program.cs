b2555d0792ad461d7fa7ffe8c80ab86728b36648
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2022-11-20 19:09:46 +0100

    added some katas

---
+++ 4kyu/Range Extraction/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+A format for expressing an ordered list of integers is to use a comma separated list of either
+
+    individual integers
+    or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. 
+	The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. 
+	For example "12,13,15-17"
+
+Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+
+Example:
+
+solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+// returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+*/
+using System.Collections.Generic;
+
+public class RangeExtraction
+{
+  public static string Extract(int[] args)
+  {    
+    List<int> nums = new List<int>{args[0]};
+    List<string> res = new List<string>();
+     
+    for (int i = 1; i < args.Length; i++)
+    {
+      if (args[i] == args[i - 1] + 1)
+      { 
+        nums.Add(args[i]);
+        if (i != args.Length - 1)
+          continue;
+      }
+      
+      if (nums.Count == 1)
+        res.Add(nums[0].ToString());
+      else if (nums.Count == 2)
+        res.Add(nums[0] + "," + nums[1]);
+      else
+        res.Add(nums[0] + "-" + nums[nums.Count - 1]);
+        
+      if (i == args.Length - 1 && nums[nums.Count - 1] != args[i])
+        res.Add(args[i].ToString());
+
+      nums.Clear();
+      nums.Add(args[i]);  
+    } 
+
+    return string.Join(",", res);
+  }
+}
---
+++ 5kyu/Maximum subarray sum/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+maxSequence [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+-- should be 6: [4, -1, 2, 1]
+
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. 
+If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+*/
+public static class Kata
+{
+  public static int MaxSequence(int[] arr) 
+  { 
+    if (arr.Length == 0 || arr == null)
+      return 0;
+    
+    int sum = 0;
+    int res = 0;
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+      for (int j = i; j < arr.Length; j++)
+      {  
+        sum += arr[j];
+        
+        if (sum > res)
+          res = sum;
+      }
+      sum = 0;
+    }
+    
+    return res;
+  }
+}
