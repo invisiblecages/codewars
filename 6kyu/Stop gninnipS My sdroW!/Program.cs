5606dde7530a6c1324a0e13c13fbc72658531b43
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2022-11-24 15:38:16 +0100

    added more katas

---
+++ 6kyu/Stop gninnipS My sdroW!/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+*/
+public class Kata
+{
+  public static string SpinWords(string sentence)
+  {
+    string[] arr = sentence.Split();
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+      string rev = "";
+      
+      if (arr[i].Length >= 5)
+      {
+        for (int j = arr[i].Length - 1; j >= 0; j--)
+          rev += arr[i][j];
+        
+        arr[i] = rev;
+      }
+    }
+    
+    return string.Join(" ", arr);
+  }
+}
---
+++ 6kyu/Write Number in Expanded Form/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Write Number in Expanded Form
+
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+Kata.ExpandedForm(12); # Should return "10 + 2"
+Kata.ExpandedForm(42); # Should return "40 + 2"
+Kata.ExpandedForm(70304); # Should return "70000 + 300 + 4"
+
+NOTE: All numbers will be whole numbers greater than 0.
+*/
+using System.Collections.Generic;
+
+public static class Kata 
+{
+  public static string ExpandedForm(long num) 
+  {
+    List<long> nums = new List<long>();
+    long pv = 1;
+    
+    while (num > 0)
+    {
+      long digit = num % pv;
+      
+      if (digit != 0)
+        nums.Add(digit);
+        
+      num -= digit;
+      pv *= 10;
+    }
+    
+    nums.Reverse();
+    return string.Join(" + ", nums);
+  }
+}
