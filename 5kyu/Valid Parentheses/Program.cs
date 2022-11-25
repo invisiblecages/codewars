8feb441ddfb96f2c165ad3f7850df9979c6a3991
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2022-11-25 07:18:31 +0100

    added more katas

---
+++ 5kyu/Valid Parentheses/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. 
+The function should return true if the string is valid, and false if it's invalid.
+Examples
+
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+
+Constraints
+
+0 <= input.length <= 100
+
+Along with opening (() and closing ()) parenthesis, input may contain any valid ASCII characters. 
+Furthermore, the input string may be empty and/or not contain any parentheses at all. 
+Do not treat other forms of brackets as parentheses (e.g. [], {}, <>).
+*/
+public class Parentheses
+{
+  public static bool ValidParentheses(string input)
+  {
+    int opening = 0;
+    int closing = 0;
+    
+    foreach (char c in input)
+    {
+      if (c == ')')
+        closing++;
+      if (c == '(')
+        opening++;
+      
+      if (closing > opening)
+        return false;   
+    }
+
+    return opening == closing;
+  }
+}
---
+++ 6kyu/Calculate String Rotation/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ Write a function that receives two strings and returns n, 
+ where n is equal to the number of characters we should shift the first string forward to match the second. 
+ The check should be case sensitive.
+
+For instance, take the strings "fatigue" and "tiguefa". 
+In this case, the first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
+If the second string isn't a valid rotation of the first string, the method returns -1.
+Examples:
+
+"coffee", "eecoff" => 2
+"eecoff", "coffee" => 4
+"moose", "Moose" => -1
+"isn't", "'tisn" => 2
+"Esham", "Esham" => 0
+"dog", "god" => -1
+*/
+public class CalculateStringRotation
+{
+  public static int ShiftedDiff(string first, string second)
+  {
+    for (int i = 0; i < first.Length; i++)
+    {
+      if (first == second)
+        return i;
+
+      first = first.Substring(first.Length - 1) + first.Substring(0, first.Length - 1);
+    }
+    
+    return -1;
+  }
+}
