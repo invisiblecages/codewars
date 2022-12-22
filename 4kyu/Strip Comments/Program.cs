6c8326e2abf6a9a87cfd81d5434e8b3d9f403bba
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2022-12-22 11:22:10 +0100

    added more katas

---
+++ 4kyu/Strip Comments/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Complete the solution so that it strips all text that follows any of a set of comment markers passed in. 
+Any whitespace at the end of the line should also be stripped out.
+
+Example:
+
+Given an input string of:
+
+apples, pears # and bananas
+grapes
+bananas !apples
+
+The output expected would be:
+
+apples, pears
+grapes
+bananas
+
+The code would be called like so:
+
+string stripped = StripCommentsSolution.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new [] { "#", "!" })
+// result should == "apples, pears\ngrapes\nbananas"
+*/
+using System;
+
+public class StripCommentsSolution
+{
+  public static string StripComments(string text, string[] commentSymbols)
+  {
+    var res = text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+    
+    for (int i = 0; i < res.Length; i++)
+    {
+      foreach (var symbol in commentSymbols)
+        if (res[i].Contains(symbol))
+          res[i] = res[i].Substring(0, res[i].IndexOf(symbol));
+      
+      res[i] = res[i].TrimEnd();
+    }
+    
+    return string.Join("\n", res);
+  }
+}
---
+++ 6kyu/Darwin Theory/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+#Darwin Theory
+
+The story of this Kata is similar to Darwin Theory.
+There is a class named "Creature" and you are supposed to complete the class. Any instance of this class is a creature.
+
+Lets say:
+
+Creature balineseCat = new Creature();
+
+balineseCat is a cat that you have created and you are the True God.
+
+You have to be able to assign your creature properties by use of indexer, for example:
+
+balineseCat["Eye"]="Blue";
+
+It is not the whole story yet.
+Suppose you create a fish:
+
+Creature fish= new Creature();
+
+give it the ability to survive in water:
+
+fish["gill"]=true;
+
+Then you create a frog:
+
+Creature frog= new Creature();
+
+The frog can automatically survive in water, and you don't need to modify it further.
+If you give him lung to breathe in air and drop the gill:
+
+frog["lung"]=true;
+frog["gill"]=false;
+
+The next creature you will create can automatically breathe in air with the lung.
+
+P.S. The properties that are assigned by indexers can be in any type.
+Test cases clarify the problem.
+Take a look at MSDN to get more help about indexers:
+https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/indexers/?redirectedfrom=MSDN
+*/
+using System.Collections.Generic;
+
+public class Creature
+{
+  private static readonly Dictionary<string, object> properties = new();
+  
+  public object this[string property]
+  {
+    get { return properties[property]; }
+    set { properties[property] = value; }
+  }
+}
